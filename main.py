4bb273f017d1234101f8acfdf0d5d9ad70ea6ec4
Aleksandr Shpuntenko <shpuntenko.a@gmail.com>
2021-11-26 11:17:35 +1000

    Revert "WIP parsing of workouts page with all the plans. Added 'exportDir' into the settings file."
    This reverts commit 22607e9fdd3075f275d1d6bca44740155c3cf90a.

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-import json
-from xml.etree.ElementTree import parse 
-with open('settings.json') as f: 
-    settings = json.load(f)
+import json 
+with open('options.json') as f: 
+    options = json.load(f)
     
 urls = [] 
-if 'urls' in settings: 
-    urls.extend(settings['urls'])
+if 'urls' in options: 
+    urls.extend(options['urls'])
 
-export_dir = settings['exportDir']
-plans_url = settings['plansUrl'] 
-from parsing import parse_plans
-parse_plans(plans_url, export_dir)
-
-import sys 
-if __name__ == '__main__': 
-    print(sys.argv)
+from parsing import save_workout
+for url in urls: 
+    save_workout(url)
